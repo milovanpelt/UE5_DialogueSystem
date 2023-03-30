6c3d7c7b2b8c71bfcc931d03027dd0a5e4a5752e
191484 <191484@buas.nl>
2023-03-30 12:09:05 +0200

    [ADDED] UE5 Project

---
+++ Source/UE5_DialogueSystem/UE5_DialogueSystem.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UE5_DialogueSystem : ModuleRules
+{
+	public UE5_DialogueSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/UE5_DialogueSystem/UE5_DialogueSystemGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_DialogueSystemGameMode.h"
+#include "UE5_DialogueSystemCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUE5_DialogueSystemGameMode::AUE5_DialogueSystemGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
