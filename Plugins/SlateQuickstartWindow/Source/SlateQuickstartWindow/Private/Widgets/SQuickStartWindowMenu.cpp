d515bb81ec1a985343746d0708f18e6a06c62ba4
191484 <191484@buas.nl>
2023-06-06 23:55:31 +0200

    [ADDED] Slate window menu

---
+++ Plugins/SlateQuickstartWindow/Source/SlateQuickstartWindow/Private/Widgets/SQuickStartWindowMenu.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/SQuickStartWindowMenu.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SQuickStartWindowMenu::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
