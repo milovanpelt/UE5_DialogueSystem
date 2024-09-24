0de02b022dc2f745c05c5f5b7a3aa696d128e030
Milo van Pelt <milovanpelt@gmail.com>
2024-09-24 14:02:02 +0200

    Revert "[ADDED] DialogueSession"
    This reverts commit a4b9ef705bcfff8e2250ac4128801d354e840893.

--- Source/UE5_DialogueSystem/UE5_DialogueSystem.Build.cs
+++ Source/UE5_DialogueSystem/UE5_DialogueSystem.Build.cs
@@ -11,7 +11,5 @@ public class UE5_DialogueSystem : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore"});
-
-        PrivateDependencyModuleNames.AddRange(new string[] { "GenericGraphRuntime" });
     }
 }
