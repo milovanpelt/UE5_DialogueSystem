7207b2e120a15ab5d7320af22c09b82b32819904
Milo van Pelt <milovanpelt@gmail.com>
2024-09-26 11:26:14 +0200

    [ADDED] Dialogue Trigger box

---
+++ Source/UE5_DialogueSystem/Game/DialogueTriggerBox.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "DialogueTriggerBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_DIALOGUESYSTEM_API ADialogueTriggerBox : public ATriggerBox
+{
+	GENERATED_BODY()
+};
