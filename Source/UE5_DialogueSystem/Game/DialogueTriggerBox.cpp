e9c4e1eb8ff71eedb2190354856071bbbfd5f6d8
Milo van Pelt <milovanpelt@gmail.com>
2024-09-26 14:52:30 +0200

    [ADDED] Trigger box is collidable

--- Source/UE5_DialogueSystem/Game/DialogueTriggerBox.cpp
+++ Source/UE5_DialogueSystem/Game/DialogueTriggerBox.cpp
@@ -3,3 +3,21 @@
 
 #include "DialogueTriggerBox.h"
 
+
+ADialogueTriggerBox::ADialogueTriggerBox()
+{
+	OnActorBeginOverlap.AddDynamic(this, &ADialogueTriggerBox::Event);
+}
+
+void ADialogueTriggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADialogueTriggerBox::Event(AActor* overlappedActor, AActor* otherActor)
+{
+	if (otherActor && otherActor != this)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Dialogue Box"));
+	}
+}
--- Source/UE5_DialogueSystem/Game/DialogueTriggerBox.h
+++ Source/UE5_DialogueSystem/Game/DialogueTriggerBox.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "Engine/TriggerBox.h"
 #include "DialogueTriggerBox.generated.h"
 
@@ -13,4 +14,11 @@ UCLASS()
 class UE5_DIALOGUESYSTEM_API ADialogueTriggerBox : public ATriggerBox
 {
 	GENERATED_BODY()
+protected:
+	virtual void BeginPlay();
+public:
+	ADialogueTriggerBox();
+
+	UFUNCTION()
+		void Event(class AActor* overlappedActor, class AActor* otherActor);
 };
